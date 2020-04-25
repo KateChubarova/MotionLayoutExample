f4a9103a15268f57fdbdd18b5d7ad8145a4f42c4
Kate Chubarova <k.chubarava@altoros.com>
2020-04-25 13:21:18 +0300

    feat(motion) : don't work 1. can not open R.java file 2. imageview does not appear in app

--- app/src/main/java/com/crocodile/motionlayoutexample/MainActivity.kt
+++ app/src/main/java/com/crocodile/motionlayoutexample/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.crocodile.motionlayoutexample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+/*TODO: если этот класс появился с 14 апи то как его получить не использую constraint layout dependency 2.0.beta? */
+//motion scene can be set up programmatically
+//contains : constraintSets, transitions + keyFrames
+//Android studio 4.0 beta editing&previewing MotionScene files
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<ImageView>(R.id.button)
     }
 }
