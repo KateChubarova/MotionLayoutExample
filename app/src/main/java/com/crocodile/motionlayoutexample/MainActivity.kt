d0526fb7164dea5c11cb95d8195294d1181f0135
Kate Chubarova <k.chubarava@altoros.com>
2020-04-26 23:52:56 +0300

    feat(motion) : fix bug with setLayout 1. can not open R.java file

--- app/src/main/java/com/crocodile/motionlayoutexample/MainActivity.kt
+++ app/src/main/java/com/crocodile/motionlayoutexample/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_motion_layout)
         findViewById<ImageView>(R.id.button)
     }
 }
